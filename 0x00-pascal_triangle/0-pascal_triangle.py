e183e7a53eff97e8a90ce506055e408b02d496f5
attamakevin <kevinejikez@gmail.com>
2023-09-27 23:35:11 +0000

    paschal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -8,7 +8,7 @@ def pascal_triangle(n):
     """
     new_list = []
     if n <= 0:
-        return new_list
+        print (new_list)
 
     for x in range(n):
         new_list.append("".join(map(str, str(11**x))))
