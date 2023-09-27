a368e3e14a212d0771a40812d466a6344cbbc64a
attamakevin <kevinejikez@gmail.com>
2023-09-27 22:45:48 +0000

    paschal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-#implementation of paschal triangle
+"""A script to determine pascal's triangle for any number"""
 def pascal_triangle(n):
-    new_list = []
+    """
+    returns a list of lists of integers representing the Pascal’s triangle of n
+    """
+    new_list =[]
     if n <= 0:
         return new_list
-    else:
-        for x in range(n):
-            new_list.append("".join(map(str, str(11**x))))
-        return new_list
+
+    for x in range(n):
+        new_list.append("".join(map(str, str(11**x))))
+    return new_list
 
