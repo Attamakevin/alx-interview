76fe5d40a748c1dd67e7374b336a1f36f3cedf15
attamakevin <kevinejikez@gmail.com>
2023-10-05 21:24:15 +0000

    done

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''
+     a method that determines if all the boxes can be opened.
+'''
+def canUnlockAll(boxes):
+
+    ''''
+        box visit method
+    '''
+    def dfs(box_index, visited):
+
+        visited[box_index] = True
+        for key in boxes[box_index]:
+            if not visited[key]:
+                dfs(key, visited)
+
+    n = len(boxes)
+    visited = [False] * n
+    dfs(0, visited)
+
+    return all(visited)
