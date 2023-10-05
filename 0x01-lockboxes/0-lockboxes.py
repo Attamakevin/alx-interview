0fd73ffec5916d0605e1dd54d024d8847be8bdee
attamakevin <kevinejikez@gmail.com>
2023-10-05 21:42:48 +0000

    done

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3
 '''
-     a method that determines if all the boxes can be opened.
+     a method that determines if all the boxes can be opened
 '''
 
 
 def canUnlockAll(boxes):
 
-    ''''
-        box visit method
-    '''
-    def dfs(box_index, visited):
-
-        visited[box_index] = True
-        for key in boxes[box_index]:
-            if not visited[key]:
-                dfs(key, visited)
+    if not boxes:
+        return False
 
     n = len(boxes)
     visited = [False] * n
-    visited[0] = True
+    visited[0] = True  # The first box is unlocked initially
 
-    dfs(0, visited)
+    stack = [0]  # Start with the first box in the stack
+
+    while stack:
+        current_box = stack.pop()
+        for key in boxes[current_box]:
+            if 0 <= key < n and not visited[key]:
+                visited[key] = True
+                stack.append(key)
 
     return all(visited)
