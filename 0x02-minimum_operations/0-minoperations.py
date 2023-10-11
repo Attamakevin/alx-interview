57ff1d2baf12637997e001787d36aa28dd87915c
attamakevin <kevinejikez@gmail.com>
2023-10-11 10:04:02 +0000

    done

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'''
+    a funcion to dtermine the minimum operation on a given array 
+    of n contents
+'''
+def minOperations(n):
+    '''minimum number of operations methods'''
+    if n <= 1:
+        return 0  # It's impossible to achieve less than 2 'H' characters.
+
+    operations = 0
+    clipboard = 0
+    current_h = 1
+
+    while current_h < n:
+        if n % current_h == 0:
+            clipboard = current_h
+            operations += 1
+
+        current_h += clipboard
+        operations += 1
+
+    if current_h == n:
+        return operations
+    else:
+        return 0  # It's impossible to achieve exactly n 'H' characters.
+
