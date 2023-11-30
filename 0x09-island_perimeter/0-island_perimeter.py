f1d14cd5834127fabf016ca2916aa8d15c82c5cb
attamakevin <kevinejikez@gmail.com>
2023-11-30 14:30:01 +0000

    done

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 
+def check(x):
+    if x != 0:
+        return 0
+    return 1
+
 
 """Island Perimeter - ALX Interview"""
 
@@ -8,26 +13,32 @@ def island_perimeter(grid):
     if not grid or not grid[0]:
         return 0
 
-    rows, cols = len(grid), len(grid[0])
-    perimeter = 0
+    row = len(grid)
+    col = len(grid[0])
+    assert (1 <= row and col <= 100), "length must be between 1 an 100"
 
-    for i in range(rows):
-        for j in range(cols):
+    x = 0
+    for i in range(row):
+        for j in range(col):
+            assert (grid[i][j] == 0) or (grid[i][j] == 1),\
+                                        "grid numbers must be 0 or 1"
             if grid[i][j] == 1:
-                perimeter += 4
-                """Each land cell contributes 4
-                sides to the perimeter
-                Check neighbors (up, down, left, right)
-                and subtract 1 for
-                each adjacent land cell
-                """
-                if i > 0 and grid[i - 1][j] == 1:
-                    perimeter -= 1
-                if i < rows - 1 and grid[i + 1][j] == 1:
-                    perimeter -= 1
-                if j > 0 and grid[i][j - 1] == 1:
-                    perimeter -= 1
-                if j < cols - 1 and grid[i][j + 1] == 1:
-                    perimeter -= 1
+                if i-1 < 0:
+                    x += 1
+                else:
+                    x += check(grid[i-1][j])
+                if j-1 < 0:
+                    x += 1
+                else:
+                    x += check(grid[i][j-1])
 
-    return perimeter
+                try:
+                    x += check(grid[i+1][j])
+                except IndexError:
+                    x += 1
+                try:
+                    x += check(grid[i][j+1])
+                except IndexError:
+                    x += 1
+
+    return x
